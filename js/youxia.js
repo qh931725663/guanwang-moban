95706f68a07906d90d78a958210fb6ddb7351fc6
qh931725663 <931725663@qq.com>
2017-11-25 13:51:46 +0800

    企业官网模版

---
+++ js/youxia.js
@@ -0,0 +1,3 @@
+/*ящй╬сроб╫г*/
+var cpro_id = "u2956265";
+document.write('<scri'+'pt src="../../cpro.baidustatic.com/cpro/ui/c.js"/*tpa=http://cpro.baidustatic.com/cpro/ui/c.js*/ type="text/javascript"></sc'+'ript>');
